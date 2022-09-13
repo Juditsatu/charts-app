8783ede47f2057bc651de46b0ce4b89ffce5acd3
Judit Santos <judit.sant@gmail.com>
2022-09-13 21:26:39 +0200

    finished doughnut

--- src/app/charts/pages/doughnut/doughnut.component.ts
+++ src/app/charts/pages/doughnut/doughnut.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-doughnut',
@@ -6,11 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DoughnutComponent implements OnInit {
+export class DoughnutComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+   // Doughnut
+   public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+   public doughnutChartData: ChartData<'doughnut'> = {
+     labels: this.doughnutChartLabels,
+     datasets: [
+       { data: [ 350, 450, 100 ], backgroundColor: '#FF5F5D', hoverBackgroundColor: '#FF9933' },
+       { data: [ 50, 150, 120 ], backgroundColor: '#3F7C85', hoverBackgroundColor: '#0099DD' },
+       { data: [ 250, 130, 70 ], backgroundColor: '#00CCBF', hoverBackgroundColor: '#00ABBD' }
+     ]
+   };
+   public doughnutChartType: ChartType = 'doughnut';
 
 }
